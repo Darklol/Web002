d874cbc760557d52dce4bf2501f5fa18345fc194
hsy <975892929@qq.com>
2020-11-10 17:50:26 +0800

    Final version!

--- web/js/table.js
+++ web/js/table.js
@@ -1,22 +1,41 @@
 
+function getResult(x,y,r){
+    //Quarter part
+    let firstQuarter = x >= 0 && y >= 0;
+    let thirdQuarter = x <= 0 && y <= 0;
+    let fourthQuarter = x >= 0 && y <= 0;
+    //check part
+    return (fourthQuarter && (x<=r/2) && (y>=-r)) ||
+        (thirdQuarter && (x*x + y*y <= r*r/4)) ||
+        (firstQuarter && (y <= -x + r/2));
+}
 function drawPointsFromTableData() {
     let newR;
-    let i = 0;
+    let rFlag = false;
     $(".table-row").each(function () {
         const query = $(this);
         const x = parseFloat(query.find(">:first-child").text());
         const y = parseFloat(query.find(">:nth-child(2)").text());
         const r = parseFloat(query.find(">:nth-child(3)").text());
-        if(i === 0){
+        // let color = query.find(">:nth-child(5)").css("color");
+        if(!rFlag){
             newR = r;
-            i = 1
+            rFlag = true;
         }
-        const color = query.find(">:nth-child(5)").css("color");
+        const existingContent = plot.html();
+        const contentToInsert1 = `<circle r="4" cx="${fromRToSvgX(x, newR)}" cy="${fromRToSvgY(y, newR)}" fill="green"></circle>`;
+        const contentToInsert2 = `<circle r="4" cx="${fromRToSvgX(x, newR)}" cy="${fromRToSvgY(y, newR)}" fill="red"></circle>`;
+        if (getResult(x,y,newR)){
+            plot.html(existingContent + contentToInsert1);
+        }else {
+            plot.html(existingContent + contentToInsert2);
+        }
+
+
 
         // insert new dot))))
-        const existingContent = plot.html();
-        const contentToInsert = `<circle r="4" cx="${fromRToSvgX(x, newR)}" cy="${fromRToSvgY(y, newR)}" fill="${color}"></circle>`;
-        plot.html(existingContent + contentToInsert);
+
+        // plot.html(existingContent + contentToInsert);
     });
 }
 
